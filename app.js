ce130f4c1207540afd5696b8b363439e4d4a3da5
Thomas Rehnert <tomrehnert2011@gmail.com>
2015-10-13 16:09:41 -0700

    99 percent done

--- app.js
+++ app.js
@@ -36,4 +36,25 @@ app.controller("PokemonCtrl", function($scope) {
 	  }
 	];
 
+
 });
+
+app.filter("reverse", function() {
+  return function(input) {
+    var result = "";
+    input = input || "";
+    for (var i=0; i<input.length; i++) {
+      result = input.charAt(i) + result;
+    }
+
+    return result.toLowerCase();
+  };
+});
+
+
+
+
+
+
+
+
